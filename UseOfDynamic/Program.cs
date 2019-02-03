30ce9a9d871092916c864e7d30ff0371a6d2fa9c
TheAnilDeveloper <anilsri.04@gmail.com>
2019-02-03 13:25:21 +0530

    Update Program.cs

--- UseOfDynamic/Program.cs
+++ UseOfDynamic/Program.cs
@@ -10,6 +10,24 @@ namespace UseOfDynamic
     {
         static void Main(string[] args)
         {
+             static void Main(string[] args)
+            {
+            //Overcoming Compile time problem related with var
+            //eg var "Hello World! was the first Program Written By Sir Dennis Ritchie !";
+            //i=false;
+            dynamic dynamicVariable = 200;
+                Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+               dynamicVariable = "Hello World! was the first Program Written By Sir Dennis Ritchie !";
+            
+                Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+                dynamicVariable = false;
+                Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+                dynamicVariable = DateTime.Now;
+                Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+            }
         }
     }
 }
